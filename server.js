4852f417f1180a796c75da86a2a46ba966487d60
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-11-06 09:30:57 -0500

    Installed colors to highlight console.logs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: './config/config.env' });
 const port = process.env.PORT || 3000;
 const bootcamps = require('./routes/bootcamps');
 const logger = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 const app = express();
 
@@ -19,12 +20,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 //Start Server
 const server = app.listen(port, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold);
 });
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
 
   //Close Server & exit process
   server.close(() => process.exit(1));
