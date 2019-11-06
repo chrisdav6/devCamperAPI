57f46b93707741face243af0571734f98f353bb1
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-11-06 09:27:08 -0500

    Installed mongoose and configured connection to MongoDbAtlas cluster

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: './config/config.env' });
 const port = process.env.PORT || 3000;
 const bootcamps = require('./routes/bootcamps');
 const logger = require('morgan');
+const connectDB = require('./config/db');
 const app = express();
 
 //Dev Logging Middleware
@@ -10,10 +11,21 @@ if (process.env.NODE_ENV === 'development') {
   app.use(logger('dev'));
 }
 
+//Connect to Database
+connectDB();
+
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 //Start Server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  //Close Server & exit process
+  server.close(() => process.exit(1));
 });
