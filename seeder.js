34f4a04e8188f36894132b636cfa6214d8476653
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-11-07 15:09:56 -0500

    Created Courses mongoose model and updated seeder.js to populate courses on the DB

--- seeder.js
+++ seeder.js
@@ -5,19 +5,23 @@ const mongoose = require('mongoose');
 const colors = require('colors');
 const connectDB = require('./config/db');
 
-//Load Model
+//Load Models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 //Connect to DB
 connectDB();
 
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
 //Clear DB then save new data
 const importData = asyncHandler(async () => {
   await Bootcamp.deleteMany();
+  await Course.deleteMany();
   await Bootcamp.create(bootcamps);
+  await Course.create(courses);
   console.log(`Old data deleted!`.red.inverse);
   console.log(`New data Seeded!`.green.inverse);
   mongoose.connection.close();
