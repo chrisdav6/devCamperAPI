05672cf0b46c68a62eb6696290d501e97257f5d6
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-11-20 14:08:37 -0500

    Rate Limiting, HPP & CORS

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const app = express();
@@ -40,6 +43,19 @@ app.use(helmet());
 
 //Prevent XSS Attacks
 app.use(xss());
+
+//Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 100, //10 minutes
+  max: 150
+});
+app.use(limiter);
+
+//Prevent http Param Polution
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 //Set Static Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
